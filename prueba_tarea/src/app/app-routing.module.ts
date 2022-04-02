91803ed2f2057e902c3520bd2f03476e338aee6c
Aldo Valle <alskabeat@gmail.com>
2022-04-02 10:42:32 -0600

    Avance de proyecto numero 2

--- prueba_tarea/src/app/app-routing.module.ts
+++ prueba_tarea/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ElementosComponent } from './components/elementos/elementos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Inicio', component: InicioComponent},
+  {path: 'Elementos', component: ElementosComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'Inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- prueba_tarea/src/app/app.module.ts
+++ prueba_tarea/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ElementosComponent } from './components/elementos/elementos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ElementosComponent
   ],
   imports: [
     BrowserModule,
